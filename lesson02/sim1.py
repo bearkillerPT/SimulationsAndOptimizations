61b98172f9644d4225c30234dd869d8d3c7aeddd
BearkillerPT <gilteixeira@ua.pt>
2022-03-30 23:17:31 +0100

    #itworks

--- lesson02/sim1.py
+++ lesson02/sim1.py
@@ -9,7 +9,7 @@ import os
 
 
 class Company:
-    def __init__(self, s, S, debug):
+    def __init__(self, s, S, order_incremetal_cost):
         self.sim_time = 0.0
 
         # state variables
@@ -19,26 +19,26 @@ class Company:
         self.quantity_backlogged = 0
         self.time_last_event = 0.0
         self.ordered_quantity = 0
+        self.order_incremetal_cost = order_incremetal_cost
 
         # statistics
-
-        # random.choices([1,2,3,4], [1/6,1/3,1/3,1/6], k=1) order size
+        self.shortage_cost = 0
+        self.handling_cost = 0
+        self.ordering_cost = 0
 
         # event list
         self.time_next_event = {}
-        self.time_next_event['order_arrival'] = self.sim_time + \
-            random.uniform(0.5, 1)
+        self.time_next_event['order_arrival'] = 121
         self.time_next_event['client_demand'] = self.sim_time + \
             random.expovariate(1/0.1)
         self.time_next_event['evaluation'] = 1
         self.time_next_event['end_simulation'] = 120
         self.next_event_type = ''
         self.time_arrival = []
-        self.debug = debug
 
     def timing(self):
 
-        min_time_next_event = 1
+        min_time_next_event = 121
 
         for e in self.time_next_event:
             if self.time_next_event[e] < min_time_next_event:
@@ -48,46 +48,63 @@ class Company:
         self.sim_time = min_time_next_event
 
     def order_arrival(self):
-        self.inventory_level += self.ordered_quantity
-        self.time_next_event['order_arrival'] = 0
+        if self.quantity_backlogged > 0:
+            inventory_increment = self.ordered_quantity - self.quantity_backlogged
+            if inventory_increment >= 0:
+                self.inventory_level += inventory_increment
+                self.quantity_backlogged = 0
+            else:
+                self.quantity_backlogged -= self.ordered_quantity
+        else:      
+            self.inventory_level += self.ordered_quantity
+
+        self.time_next_event['order_arrival'] = 121
 
     def client_demand(self):
-        pass
+        [quantity_demanded] = random.choices(
+            [1, 2, 3, 4], [1/6, 1/3, 1/3, 1/6], k=1)
+        if self.inventory_level >= quantity_demanded:
+            self.inventory_level -= quantity_demanded
+        else:
+            self.quantity_backlogged += quantity_demanded
+
+        self.time_next_event['client_demand'] = self.sim_time + \
+                random.expovariate(1/0.1)
 
     def evaluation(self):
         if self.inventory_level < self.s:
-            quantity_to_order = self.S - self.inventory_level
+            self.ordered_quantity = self.S - self.inventory_level
+            self.time_next_event['order_arrival'] = self.sim_time + \
+                random.uniform(0.5, 1)
+            self.ordering_cost += 32 + self.order_incremetal_cost*self.ordered_quantity
+        if self.quantity_backlogged > 0:
+            self.shortage_cost += 5*self.quantity_backlogged
+        if self.inventory_level > 0:
+            self.handling_cost += self.inventory_level
+
+        #print("Inventory: " + str(self.inventory_level))
+        #print("Backlog: " + str(self.quantity_backlogged))
+
 
         self.time_next_event['evaluation'] += 1
 
-
-
-    def end_simulation(self):
-        pass
-
     def run(self):
-        while self.num_custs_delayed < 5:
+        while self.sim_time < 120:
             self.timing()
-            if self.next_event_type == 'arrive':
-                self.arrive()
-            elif self.next_event_type == 'depart':
-                self.depart()
+            if self.next_event_type == 'order_arrival':
+                self.order_arrival()
+            elif self.next_event_type == 'client_demand':
+                self.client_demand()
+            elif self.next_event_type == 'evaluation':
+                self.evaluation()
+
 
     def show_stats(self):
-        print("\nDelay per costumer:")
-        print(self.clients_statistics)
-        clients = len(self.clients_statistics.keys())
-        avg_delay = 0
-        if(clients):
-            print("\nAvg delay:")
-            avg_delay = statistics.mean(self.clients_statistics.values())
-            print(avg_delay)
-        avg_queue_size = statistics.mean(self.size_of_queue)
-        print("\nAvg queue size:")
-        print(avg_queue_size)
-        print("\nServer Frequency:")
-        server_freq = self.clients_done/self.sim_time
-        print(server_freq)
+        print("Shortage Cost:"+ str(self.shortage_cost))
+        print("Handling Cost:"+ str(self.handling_cost))
+        print("Ordering Cost:"+ str(self.ordering_cost))
+        print("Total Cost:" +str(self.shortage_cost + self.handling_cost + self.ordering_cost))
+
 
     def save_stats(self, filename):
         clients = len(self.clients_statistics.keys())
@@ -113,7 +130,7 @@ def main():
     # initialize
     # simulation clock
     if len(sys.argv) == 1:
-        company = Company(True)
+        company = Company(20,40, 3)
         company.run()
         company.show_stats()
     elif len(sys.argv) == 2:
