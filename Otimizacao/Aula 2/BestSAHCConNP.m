a9caad8b573c2b39c0715caeb3802a9c2aa83a9e
BearkillerPT <gilteixeira@ua.pt>
2022-05-25 21:49:56 +0100

    added more stuff for ex4

--- Otimizacao/Aula 2/BestSAHCConNP.m
+++ Otimizacao/Aula 2/BestSAHCConNP.m
@@ -3,23 +3,30 @@ L= load('L2.txt');
 G=graph(L);
 N = numnodes(G);
 n = length(current_best_n);
-t=tic;
 servers= current_best_n;
 min_ConNP = ConnectedNP(G,current_best_n);
-while toc(t)<search_time
+improved = true;
+while improved
+    last_improvement = min_ConNP;
     if neighbor_type == "def1Neighbors"
         others_connp= def1Neighbors(G,servers);
-        servers_connp=[servers(randperm(n,n-1)) others_connp(randperm(N-n,1))];
+        next_servers = servers(randperm(n,n-1));
     else
-        next_servers= servers(randperm(n,n-1))
+        next_servers= servers(randperm(n,n-1));
         others_connp= def2Neighbors(G,servers,setdiff(servers,next_servers));
-        servers_connp=[next_servers others_connp]
+        
     end
-    ConNP= ConnectedNP(G,servers_connp);
-    if(ConNP < min_ConNP)
-        min_ConNP = ConNP;
-        servers= servers_connp;
-    end    
+    for other = others_connp
+        servers_connp=[next_servers other]
+        ConNP= ConnectedNP(G,servers_connp);
+        if(ConNP < min_ConNP)
+            min_ConNP = ConNP;
+            servers= servers_connp;
+        end   
+    end
+    if last_improvement == min_ConNP
+        improved = false;
+    end
 end
 min_servers=servers;
 end
