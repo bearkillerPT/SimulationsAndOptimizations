2243fb35723e4b10181402ebb5f9eb727f3b212a
gilteixeira <gilteixeira@ua.pt>
2022-06-14 19:45:03 +0100

    crushed the adaptiveSearch

--- Otimizacao/Project/AdaptativeSearch.m
+++ Otimizacao/Project/AdaptativeSearch.m
@@ -1,32 +1,30 @@
-function [min_ConNP,min_servers,execution_time] = AdaptativeSearch(neighbor_type)
-    t=tic;
-    L= load('L2.txt');
-    G=graph(L);
-    N = numnodes(G);
-    n = 10;
-    servers= randomperm (N, n);
-    min_ConNP = ConnectedNP(G,servers);
-    improved = true;
-    while improved
-        last_improvement = min_ConNP;
-        if neighbor_type == "def1Neighbors"
-            others_connp= def1Neighbors(G,servers);
-            next_servers = servers(randperm(n,n-1));
-        else
-            next_servers= servers(randperm(n,n-1));
-            others_connp= def2Neighbors(G,servers,setdiff(servers,next_servers));
-            
-        end
-        for other = others_connp
-            servers_connp=[next_servers other];
-            ConNP= ConnectedNP(G,servers_connp);
-            if(ConNP < min_ConNP)
-                min_ConNP = ConNP;
-                servers= servers_connp;
-            end   
-        end
-        if last_improvement == min_ConNP
-            improved = false;
-        end
+function [min_servers] = AdaptativeSearch(G, servers, neighbor_type)
+    min_ConNP = ConnectedNP(G, servers);
+    min_servers = servers;
+
+    if neighbor_type == "def1Neighbors"
+        others_connp = def1Neighbors(G, servers);
+    else
+        others_connp = def2Neighbors(G, servers, setdiff(servers, next_servers));
     end
 
+    for other = others_connp
+        servers_connp = [next_servers other];
+        ConNP = ConnectedNP(G, servers_connp);
+
+        if (ConNP < min_ConNP)
+            min_ConNP = ConNP;
+            min_servers = servers_connp;
+        end
+
+    end
+end
+
+function [neighborhood] = def1Neighbors(G, servers)
+    N = numnodes(G);
+    neighborhood = setdiff(1:N, servers);
+end
+
+function [neighborhood] = def2Neighbors(G, new_servers, candidate)
+    neighborhood = setdiff(neighbors(G, candidate).', new_servers);
+end
